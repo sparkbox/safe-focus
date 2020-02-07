4a1974d951c7c7aa073363ed850827f4d59058da
Jordan Leven <jordan@heysparkbox.com>
2020-02-06 20:48:07 -0500

    :sparkles: feat: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'eslint-config-airbnb-base'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  }
+};
